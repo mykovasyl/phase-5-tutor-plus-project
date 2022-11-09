0488485c3047ab87793fd8a48911701d2ac02f0f
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-11-09 15:59:55 -0500

    install active storage and add avatar to user_params

---
+++ db/migrate/20221109174455_create_tutor_students.rb
@@ -0,0 +1,10 @@
+class CreateTutorStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :tutor_students do |t|
+      t.references :tutor, null: false, foreign_key: true
+      t.references :student, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
