d2b9cf0d1c29362883b26b91289f900fb0b6e4c4
Mykola Vasyl'yev <mykovasyl@gmail.com>
2022-12-12 17:57:06 -0500

    update user params permit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authorize, only: :create
 
   def create
-    user = User.create!(user_params)
+    user = User.create!(new_user_params)
     session[:user_id] = user.id
     render json: user, status: :accepted
   end
@@ -11,23 +11,27 @@ class UsersController < ApplicationController
     render json: @current_user
   end
 
-  # def update
-  #   user_to_update = find_user
-  #   user_to_update.avatar.attach(params[:avatar])
-  #   user_to_update.update!(user_params)
-  #   render json: user, status: :accepted
-  # end
+  def update
+    user_to_update = find_user
+    user_to_update.avatar.attach(params[:avatar])
+    user_to_update.update!(update_user_params)
+    render json: user, status: :accepted
+  end
 
-  # def destroy
-  #   user_to_delete = find_user
-  #   user_to_delete.destroy
-  #   head :no_content
-  # end
+  def destroy
+    user_to_delete = find_user
+    user_to_delete.destroy
+    head :no_content
+  end
 
   private
 
-  def user_params
+  def new_user_params
     params.permit(:username, :password, :password_confirmation, :avatar, :headline, :subjects, :name, :grade, :email, :type)
+  end
+
+  def update_user_params
+    params.permit(:username, :password, :password_confirmation, :avatar, :headline, :name, :email, :subkects, :grade)
   end
 
   def find_user
