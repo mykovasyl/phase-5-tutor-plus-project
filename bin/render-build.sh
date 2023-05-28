27dc58da0cdc6388ce9761f5b73f99bfa6766e90
Mykola Vasyl'yev <mykovasyl@gmail.com>
2023-05-28 08:33:23 -0400

    for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,12 +2,10 @@
 # exit on error
 set -o errexit
 
-# builds the front end code
+# Add build commands for front end
 rm -rf public
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
-# builds the back end code
 bundle install
-bundle exec rake db:migrate
-bundle exec rake db:seed # if you have seed data, run this command for the initial deploy only
+bundle exec rake db:reset db:migrate db:seed DISABLE_DATABASE_ENVIRONMENT_CHECK=1
